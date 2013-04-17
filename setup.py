1dde39d85528362f733251bbc19d4766461a8a91
Michał Bachowski <michal@bachowski.pl>
2013-04-17 10:26:16 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='PyEvent',\
+    url='https://github.com/michalbachowski/pyevent',\
+    version='1.0',\
+    description='Python implementation of PHP`s (from Symfony Components) EventDispatcher',\
+    license='New BSD License',\
+    author='Michał Bachowski',\
+    author_email='michal@bachowski.pl',\
+    package_dir={'': 'src'},\
+    packages=['pyevent'])
