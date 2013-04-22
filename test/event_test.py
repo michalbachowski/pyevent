ed295411e2ad3be6c41193186887a65c4f7645ca
Michał Bachowski <michal@bachowski.pl>
2013-04-22 15:05:54 +0200

    Fixed tests to event

--- test/event_test.py
+++ test/event_test.py
@@ -18,34 +18,26 @@ from pyevent import Event
 
 class EventTestCase(unittest.TestCase):
 
-    def test_init_requires_2_arguments(self):
+    def test_init_requires_argument(self):
         err = False
         try:
-            e = Event()
+            Event()
         except TypeError:
             err = True
         self.assertTrue(err)
 
-    def test_init_requires_2_arguments_1(self):
+    def test_init_requires_argument_1(self):
         err = False
         try:
-            e = Event(None)
-        except TypeError:
-            err = True
-        self.assertTrue(err)
-
-    def test_init_requires_2_arguments_2(self):
-        err = False
-        try:
-            e = Event(None, None)
+            Event(None)
         except TypeError:
             err = True
         self.assertFalse(err)
 
-    def test_init_allows_to_pass_3_arguments(self):
+    def test_init_allows_to_pass_2_arguments(self):
         err = False
         try:
-            e = Event(None, None, {})
+            Event(None, {})
         except TypeError:
             err = True
         self.assertFalse(err)
@@ -53,55 +45,63 @@ class EventTestCase(unittest.TestCase):
     def test_init_does_not_check_input_arguments(self):
         err = False
         try:
-            e = Event(None, None, None)
+            Event(None, None)
         except:
             err = True
         self.assertFalse(err)
 
     def test_init_state(self):
-        e = Event('a', 'b')
+        e = Event('a')
         self.assertEqual('a', e.subject)
-        self.assertEqual('b', e.name)
+        self.assertIsNone(e.name)
         self.assertEqual({}, e.parameters)
-        self.assertIsNone(e.return_value)
-        self.assertFalse(e.processed)
+        self.assertFalse(e.is_processed())
+        self.assertFalse(e.is_propagation_stopped())
 
-        e = Event('a', 'b', {'a': 1})
+        e = Event('a', {'a': 1})
         self.assertEqual({'a': 1}, e.parameters)
 
     def test_mark_processed_changes_state_of_object(self):
-        e = Event(None, None)
-        self.assertFalse(e.processed)
+        e = Event(None)
+        self.assertFalse(e.is_processed())
         e.mark_processed()
-        self.assertTrue(e.processed)
+        self.assertTrue(e.is_processed())
 
-    def test_processed_is_read_only_property(self):
-        e = Event(None, None)
-        err = False
-        try:
-            e.processed = True
-        except AttributeError:
-            err = True
-        self.assertTrue(err)
+    def test_is_propagation_stopped_returns_propagation_status(self):
+        e = Event(None)
+        self.assertFalse(e.is_propagation_stopped())
+        e.stop_propagation()
+        self.assertTrue(e.is_propagation_stopped())
 
-    def test_parametrs_can_be_accessed_throught_event_instance(self):
-        e = Event(None, None, {'a': 1})
+    def test_stop_propagation_changes_propagation_status(self):
+        e = Event(None)
+        self.assertFalse(e.is_propagation_stopped())
+        e.stop_propagation()
+        self.assertTrue(e.is_propagation_stopped())
+        e.stop_propagation()
+        self.assertTrue(e.is_propagation_stopped())
+        e.start_propagation()
+        self.assertFalse(e.is_propagation_stopped())
+        e.stop_propagation()
+        self.assertTrue(e.is_propagation_stopped())
 
-        self.assertEqual(1, e['a'])
-        self.assertTrue('a' in e)
-        # event is read-only
-        err = False
-        try:
-            e['b'] = 2
-        except RuntimeError:
-            err = True
-        self.assertTrue(err)
-        err = False
-        try:
-            del e['a']
-        except RuntimeError:
-            err = True
-        self.assertTrue(err)
+    def test_start_propagation_changes_propagation_status(self):
+        e = Event(None)
+        self.assertFalse(e.is_propagation_stopped())
+        e.stop_propagation()
+        self.assertTrue(e.is_propagation_stopped())
+        e.start_propagation()
+        self.assertFalse(e.is_propagation_stopped())
+        e.start_propagation()
+        self.assertFalse(e.is_propagation_stopped())
+        e.stop_propagation()
+        self.assertTrue(e.is_propagation_stopped())
+
+    def test_parametrs_can_be_accessed_throught_parameters_variable(self):
+        e = Event(None, {'a': 1})
+
+        self.assertEqual(1, e.parameters['a'])
+        self.assertTrue('a' in e.parameters)
 
 
 if "__main__" == __name__:
