c984a54b32746031864f1bf27b6407c6051b35f3
Michał Bachowski <michal@bachowski.pl>
2013-10-17 12:26:02 +0200

    Fixed version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ del os.link
 
 setup(name='PyEvent',
     url='https://github.com/michalbachowski/pyevent',
-    version='1.0',
+    version='1.0.0',
     description='Python implementation of PHP`s (from Symfony Components) EventDispatcher',
     license='New BSD License',
     author='Michał Bachowski',
